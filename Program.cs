6e76be4c95f7ee34d91fffef3eb8f00218a0a268
CristinaDev <Cristinadeveloper93@gmail.com>
2016-07-21 14:42:24 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,53 +1,44 @@
-using System;
-using System.Collections.Generic;
-
-namespace Ejemplos
+static void Main(string[] args)
 {
-    class Program
+    int numero = 0;
+
+    Console.Write("Introduce un número: ");
+    if(Int32.TryParse(Console.ReadLine(), out numero))
     {
-        static void Main(string[] args)
+        if(numero != 0)
         {
-            int numero = 0;
+            List<int> divisores = new List<int>(); //Lista en la que almacenaremos todos los divisores.
+            int contador = 0;
+            int divisor = 0;
 
-            Console.Write("Introduce un número: ");
-            if(Int32.TryParse(Console.ReadLine(), out numero))
+            //Calculamos todos los divisores.
+            for (contador = 1; contador <= numero; contador++)
             {
-                if(numero != 0)
+                divisor = numero % contador;
+
+                if (divisor == 0)
                 {
-                    List<int> divisores = new List<int>(); //Lista en la que almacenaremos todos los divisores.
-                    int contador = 0;
-                    int divisor = 0;
-
-                    //Calculamos todos los divisores.
-                    for (contador = 1; contador <= numero; contador++)
-                    {
-                        divisor = numero % contador;
-
-                        if (divisor == 0)
-                        {
-                            divisores.Add(contador);
-                        }
-                    }
-
-                    //Podemos mostrar o no por pantalla los resultados almacenados.
-                    Console.WriteLine("Divisores de {0}: ", numero);
-                    foreach (int elemento in divisores)
-                    {
-                        Console.WriteLine("{0}", elemento);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Resultado indefinido.");
+                    divisores.Add(contador);
                 }
             }
-            else
-            {
-                Console.WriteLine("Se esperaba un número entero.");
-            }
 
-            Console.WriteLine();
-            Console.ReadKey();
+            //Podemos mostrar o no por pantalla los resultados almacenados.
+            Console.WriteLine("Divisores de {0}: ", numero);
+            foreach (int elemento in divisores)
+            {
+                Console.WriteLine("{0}", elemento);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Resultado indefinido.");
         }
     }
+    else
+    {
+        Console.WriteLine("Se esperaba un número entero.");
+    }
+
+    Console.WriteLine();
+    Console.ReadKey();
 }
